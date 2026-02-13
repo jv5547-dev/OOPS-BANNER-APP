866ca96d80f7594d393e4565ce4f28b88e018a81
J. Vanishitta <jv5547@srmist.edu.in>
2026-02-13 15:05:19 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+package OOPS_banner_app;
+
+    public class Main {
+    public static void main(String[] args) {
+       System.out.println("OOPS");
+        // System.out.println("  ***    ***   ****    **** ");
+        // System.out.println(" *   *  *   *  *   *  *     ");
+        // System.out.println(" *   *  *   *  *   *  *     ");
+        // System.out.println(" *   *  *   *  ****    ***  ");
+        // System.out.println(" *   *  *   *  *          * ");
+        // System.out.println(" *   *  *   *  *          * ");
+        // System.out.println("  ***    ***   *      ****  ");
+    }
+}
+
+
